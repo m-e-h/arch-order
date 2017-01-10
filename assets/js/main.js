400a247a2a07f380b1ecbc134bf8a5c7c3383bd8
m-e-h <info@martyhelmick.com>
2017-01-09 20:15:41 -0500

    id of app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,36 @@
 var vm = new Vue({
 
-  el: '#demo',
+	el: '#app',
 
-  data: {
-    posts: '',
-	slug: null
-  },
+	data: {
+		posts: '',
+		slug: null
+	},
 
-  created: function () {
-    this.fetchData()
-  },
+	created: function () {
+		this.fetchData()
+	},
 
-  watch: {
-	  '$route': function () {
-		  var self = this
-		  self.isLoading = true
-		  self.fetchData().then(function () {
-			  self.isLoading = false
-		  })
-	  }
-  },
+	watch: {
+		'$route': function () {
+			var self = this
+			self.isLoading = true
+			self.fetchData().then(function () {
+				self.isLoading = false
+			})
+		}
+	},
 
-  methods: {
-	  fetchData: function () {
-		  var self = this
-		  return axios.get('/wp-json/wp/v2/posts?per_page=20')
-		  .then(function (response) {
-			  self.posts = response
-		  })
-		  .catch(function (error) {
-			  self.fetchError = error
-		  })
-	  }
-  }
+	methods: {
+		fetchData: function () {
+			var self = this
+			return axios.get('/wp-json/wp/v2/posts?per_page=20')
+			.then(function (response) {
+				self.posts = response
+			})
+			.catch(function (error) {
+				self.fetchError = error
+			})
+		}
+	}
 })
